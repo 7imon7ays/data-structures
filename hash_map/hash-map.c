3b6269679834bc577c7fab4141879d90f7951f4c
Simon Chaffetz <simis1000@gmail.com>
2014-02-17 17:57:54 -0800

    Hash does everything but resize and detect duplicates

--- hash_map/hash-map.c
+++ hash_map/hash-map.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "int-array.h"
 #include "strhash.h"
 
-// 1. All constructors should take pointers so the user
-// can allocate structs on the stack or the heap.
 
-// 2. KV pairs should be structs
-
-// initialization
+// initialization with hashing and comparison
+// functions for each type.
 
 typedef struct {
   Array store;
   int numElems;
+  int maxElemsPerBucket;
+
+  int (*eq)(void*, void*);
+  int (*hash)(void*);
 } HashMap;
+
+int intEq(int* ip1, int* ip2) {
+  return (*ip1 == *ip2 ? 1 : 0);
+}
+
+int intHash(int* ip1) {
+  return SuperFastHash(ip1, sizeof(int));
+}
+
+typedef struct {
+  int x;
+  int y;
+} Coordinate;
+
+int coordinateEq(Coordinate* cp1, Coordinate* cp2) {
+  return ((cp1->x == cp2->x) || (cp1->y == cp2->y)) ? 1 : 0;
+}
+
+int coordinateHash(Coordinate* cp1) {
+  return SuperFastHash(cp1, sizeof(Coordinate));
+}
+
+int strEq(char* str1, char* str2) {
+  return strcmp(str1, str2) == 0 ? 1 : 0;
+}
+
+int strHash(char* sp1) {
+  int strLen = strlen(sp1);
+  return SuperFastHash(sp1, strLen);
+}
 
 void _makeBuckets(HashMap* hashPtr) {
   int i;
@@ -27,21 +59,27 @@ void _makeBuckets(HashMap* hashPtr) {
   }
 }
 
-void makeHashMap(HashMap* hashPtr) {
+void makeHashMap(
+      HashMap* hashPtr,
+      int (*eq)(void*, void*),
+      int (*hash)(void*)
+    ) {
+  hashPtr->numElems = 0;
+  hashPtr->maxElemsPerBucket = 1;
   Array* newStorePtr = malloc(sizeof(Array));
   hashPtr->store = *newStorePtr;
   makeArray(&hashPtr->store);
   _makeBuckets(hashPtr);
   hashPtr->numElems = 0;
+
+  hashPtr->eq = eq;
+  hashPtr->hash = hash;
 }
 
 // manipulation
 
-Array* _forEl(HashMap* hashPtr, char key) {
-  char* keyString = malloc(sizeof(char) + 1);
-  keyString[0] = key;
-  keyString[1] = '\0';
-  uint32_t hashedKey = SuperFastHash(keyString, 1);
+Array* _forEl(HashMap* hashPtr, void* keyPtr) {
+  uint32_t hashedKey = hashPtr->hash(keyPtr);
   int bucketIndex = hashedKey % hashPtr->store.storeSize;
   Array* bucketPtr = (Array*) valueAt(&hashPtr->store, bucketIndex);
 
@@ -49,61 +87,77 @@ Array* _forEl(HashMap* hashPtr, char key) {
 }
 
 typedef struct {
-  char key;
-  int value;
+  void* keyPtr;
+  void* valuePtr;
 } KVPair;
 
-// TODO: check for inclusion before adding
-void add(HashMap* hashPtr, char key, int value) {
-  Array* bucketPtr = _forEl(hashPtr, key);
-  KVPair* kvPairPtr = malloc(sizeof(KVPair));
-  kvPairPtr->key = key;
-  kvPairPtr->value = value;
 
-  push(bucketPtr, kvPairPtr);
-  Array* retrievedPairPtr = (Array*) valueAt(bucketPtr, bucketPtr->storeSize - 1);
-  printf("added pair with store address %p\n", &retrievedPairPtr->store);
-}
+void* find(HashMap* hashPtr, void* keyPtr) {
+  assert(hashPtr->store.storeSize != 0);
 
-int find(HashMap* hashPtr, char key) {
-  if (hashPtr->store.storeSize < 1) {
-    printf("hash is empty");
-    return 0;
-  }
-  Array* bucketPtr = _forEl(hashPtr, key);
+  Array* bucketPtr = _forEl(hashPtr, keyPtr);
   int i;
   for (i = 0; i < bucketPtr->storeSize; i++) {
     KVPair* thisPairPtr = (KVPair*) valueAt(bucketPtr, i);
-    if (thisPairPtr->key == key) {
-      return thisPairPtr->value;
+    if (hashPtr->eq(thisPairPtr->keyPtr, keyPtr)) {
+      return thisPairPtr->valuePtr;
     }
   }
-  printf("nothing found for key %c\n", key);
-  return 0;
+  printf("nothing found for key %s\n", (char*) keyPtr);
+  int* nullPtr = malloc(sizeof(int));
+  *nullPtr = 0;
+  return nullPtr;
+}
+
+void resizeHash(HashMap* hashPtr) {
+
+}
+
+// TODO return if key already exists with that value.
+void add(HashMap* hashPtr, void* keyPtr, void* valuePtr) {
+  if (hashPtr->numElems / hashPtr->store.storeSize >
+      hashPtr->maxElemsPerBucket) {
+    resizeHash(hashPtr);
+  }
+
+  Array* bucketPtr = _forEl(hashPtr, keyPtr);
+  KVPair* kvPairPtr = malloc(sizeof(KVPair));
+  kvPairPtr->keyPtr = keyPtr;
+  kvPairPtr->valuePtr = valuePtr;
+
+  push(bucketPtr, kvPairPtr);
+  hashPtr->numElems++;
 }
 
 int main() {
   HashMap* myHashPtr = malloc(sizeof(HashMap));
-  makeHashMap(myHashPtr);
+  makeHashMap(
+    myHashPtr,
+    (int (*)(void*, void*)) strEq,
+    (int (*)(void*)) strHash);
 
+  // add key "keyA" with value 1
+  char* key1Ptr = malloc(sizeof(char) * 5);
+  int* value1Ptr = malloc(sizeof(int*));
+  key1Ptr = "keyA";
+  *value1Ptr = 1;
+  add(myHashPtr, key1Ptr, value1Ptr);
 
-  add(myHashPtr, 'a', 1);
-  add(myHashPtr, 'b', 2);
-  add(myHashPtr, 'c', 3);
-  /*
-  add(&myHash, 'd', 4);
-  add(&myHash, 'e', 5);
-  add(&myHash, 'f', 6);
-  add(&myHash, 'g', 7);
-  */
+  // add key "keyB" with value 'b'
+  char* key2Ptr = malloc(sizeof(char) * 3);
+  char* value2Ptr = malloc(sizeof(char));
+  key2Ptr = "keyB";
+  *value2Ptr = 'b';
+  add(myHashPtr, key2Ptr, value2Ptr);
 
-  int one = find(myHashPtr, 'a');
-  int two = find(myHashPtr, 'b');
-  int three = find(myHashPtr, 'c');
+  // look up keys "keyA" and "kB". Also look up absent key.
+  int* onePtr = find(myHashPtr, "keyA");
+  char* twoPtr = find(myHashPtr, "keyB");
+  int* nonePtr = find(myHashPtr, "none");
 
-  printf("value two has key %d\n", two);
-  printf("value three has key %d\n", three);
-  printf("value one has key %d\n", one);
+  printf("key %s has value %d\n", "keyA", *onePtr);
+  printf("key %s has value %c\n", "kB", *twoPtr);
+  printf("key 'none' has value %d\n", *nonePtr);
 
   return 0;
 }
