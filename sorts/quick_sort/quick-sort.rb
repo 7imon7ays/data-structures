abec8d0d145de1692baca3a87cd1e2dbac7a3cb1
Simon Chaffetz <simis1000@gmail.com>
2014-11-18 22:06:48 -0800

    Comment quick-sort.rb

--- sorts/quick_sort/quick-sort.rb
+++ sorts/quick_sort/quick-sort.rb
@@ -1,20 +1,27 @@
 def quick_sort! arr, left = 0, right = arr.length - 1
-  return if left >= right
+  # left is greater than right when there are two
+  # els remaining and left starts at "partition + 1"
+  return if left >= right 
 
   pivot_idx = (left + right) / 2
+  # move pivot to left-most index so comparisons
+  # to remaing elements are sequential
   swap! arr, pivot_idx, left
 
-  partition = left + 1
+  num_smaller = 0
   (left + 1..right).each do |i|
     if arr[i] < arr[left]
-      swap! arr, i, partition
-      partition += 1
+      num_smaller += 1
+      swap! arr, i, left + num_smaller
     end
   end
-  partition -= 1
-  swap! arr, left, partition
-  quick_sort! arr, left, partition - 1
-  quick_sort! arr, partition + 1, right
+  
+  # right partition separates pivot's smaller
+  # els from pivot's greater els
+  right_partition = left + num_smaller
+  swap! arr, left, right_partition
+  quick_sort! arr, left, right_partition - 1
+  quick_sort! arr, right_partition + 1, right
 end
 
 def swap! arr, i, j
