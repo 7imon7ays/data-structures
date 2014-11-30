c9940c4f9daba0179e7aa68d016d438b9af5f3b9
Simon Chaffetz <simis1000@gmail.com>
2014-11-30 17:52:58 -0600

    Add spec files for iterative and recursive bsearch

---
+++ binary_search/binary-search-iter-spec.rb
@@ -0,0 +1,7 @@
+require "./binary-search-iter.rb"
+require "./binary-search-spec.rb"
+
+describe "binary search iterative" do
+  it_behaves_like "binary search"
+end
+
---
+++ binary_search/binary-search-recurs-spec.rb
@@ -0,0 +1,7 @@
+require "./binary-search-recurs.rb"
+require "./binary-search-spec.rb"
+
+describe "binary search recursive" do
+  it_behaves_like "binary search"
+end
+
